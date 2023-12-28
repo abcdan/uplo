1d8959698fe08baf377a194dd4e89dcca5e1d62f
abcdan <daniel@lngzl.nl>
2023-12-28 16:38:34 +0100

    ➕ Added dependencies

---
+++ src/uplo.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"log"
+	"os"
+	"time"
+	"uplo/helper"
+
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/limiter"
+	"github.com/joho/godotenv"
+)
+
+func main() {
+	app := fiber.New()
+
+	err := godotenv.Load()
+	if err != nil {
+		log.Println("Could not load .env file, using environment variables.")
+	}
+
+	helper.ConnectDB()
+
+	app.Static("/", "./public")
+	app.Static("/404", "./public/404.html")
+
+	app.Get("/health", func(c *fiber.Ctx) error {
+		return c.SendStatus(200)
+	})
+
+	app.Use("/api", limiter.New(limiter.Config{
+		Max:        50,
+		Expiration: 1 * time.Minute,
+	}))
+
+	app.Use(func(c *fiber.Ctx) error {
+		if c.Path() == "*" {
+			return c.SendFile("./public/404.html")
+		}
+		return c.Next()
+	})
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "3000"
+	}
+	app.Listen(":" + port)
+}
